a5961509ae59b7e7ce20bd0512a702ae64441608
lucassaporetti <lucassapooretti@gmail.com>
2020-03-21 22:43:45 -0300

    my python scripts

---
+++ script-python/phonebook.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+PROGRAM = os.path.basename(__file__).upper()
+
+MAIN_MENU = f"""
+{PROGRAM} LUCAS PERSONAL CONTACTS
+
+(1) ADD NAME
+(2) REMOVE NAME
+(3) EDIT NAME
+(4) LIST NAMES
+(5) EXIT
+"""
+
+PHONEBOOK = 'phonebook.txt'
+CONTACTS = []
+op = None
+
+with open(PHONEBOOK, 'a+') as f_phonebook : 
+
+    for line in f_phonebook:
+       (key, val) = line.split()
+       CONTACTS [int(key)] = val
+
+    while op != '5':
+
+        os.system ('cls')
+        new_contact = {"name": None, "address": None, "phone": None}
+        print (MAIN_MENU)
+        op = input ('Your Option$ ')
+
+        if op == '1':
+            new_contact['name'] = input ('What\'s your name? ')
+            new_contact['address'] = input ('What\'s your address? ')
+            new_contact['phone'] = input ('What\'s your phone? ')
+            CONTACTS.append(new_contact.copy())
+            print ('Contact added ' + str(new_contact))
+        elif op == '2':
+            name = input ('Remove who? ')
+            found = [c for c in CONTACTS if name != c['name']]
+            if len (found) != len (CONTACTS) :
+                CONTACTS = found
+                print ('Contact removed ' + name)
+            else :
+                print (f'Contact {name} not found')
+        elif op == '3':
+            name = input ('Edit who? ')
+            found = [c for c in CONTACTS if name == c['name']]
+            if len (found) > 0 : 
+                new_contact = found[0]
+                new_contact['name'] = input ('New name? ')
+                new_contact['address'] = input ('New address? ')
+                new_contact['phone'] = input ('New phone? ')
+                print ('Contact updated ' + str(new_contact))
+            else :
+                print (f'Contact {name} not found')
+        elif op == '4':
+            print (CONTACTS)
+        elif op == '5':
+            sys.exit (0)
+        else : 
+            print ('# Invalid option ' + op)
+
+        if op == '1' or op == '2' :
+            f_phonebook.write (str (CONTACTS))
+
+    input ('\n\nPress enter to continue\n')
+    os.system ('cls')
+    op = None
+   
+input ('\n\nPress enter to continue\n')
---
+++ script-python/teste01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+nome = input ('Qual e o seu nome?')
+idade = input ('Qual e a sua idade?')
+peso = input ('Qual e o seu peso?')
+print (nome, idade, peso)
